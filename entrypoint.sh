2f5914cdd71175ab131492bdfd52c3134a44fa22
Igor Ponomarev <9129910001@mail.ru>
2026-05-15 20:49:50 +0500

    Fix static files: mount staticfiles volume, collectstatic in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 exec gunicorn massaz72.wsgi:application --bind 0.0.0.0:8000 --workers 2 --threads 2 --timeout 15 --keep-alive 5
